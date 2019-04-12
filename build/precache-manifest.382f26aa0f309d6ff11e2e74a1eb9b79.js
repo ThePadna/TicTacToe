6c97d7ad0f8bdff8d0c194f545f143fc17db432c
Padna <coryf175@gmail.com>
2019-04-12 07:28:36 +0100

    added gh-pages

---
+++ build/precache-manifest.382f26aa0f309d6ff11e2e74a1eb9b79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TicTacToe/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6670768c31502717d35c",
+    "url": "/TicTacToe/static/js/runtime~main.ae000fa4.js"
+  },
+  {
+    "revision": "0076e5cca517d6a606b7",
+    "url": "/TicTacToe/static/js/main.7c7b6030.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/TicTacToe/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "0076e5cca517d6a606b7",
+    "url": "/TicTacToe/static/css/main.bb2fe3fd.chunk.css"
+  },
+  {
+    "revision": "dc13c10a5cc6add495a73fc8c41ad339",
+    "url": "/TicTacToe/index.html"
+  }
+];
