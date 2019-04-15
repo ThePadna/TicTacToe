d60d0a69c6210913ccf80564f5c20e2c208d74dc
Padna <coryf175@gmail.com>
2019-04-16 00:50:19 +0100

    Added Options Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import Options from './Options';
 
 class App extends Component {
   render() {
     return (
-        <p id="title"><span id="tic">Tic</span><span id="tac">Tac</span><span id="toe">Toe</span></p>
+      <div id="home">
+        <div id="header">
+          <p id="title"><span id="tic">Tic</span><span id="tac">Tac</span><span id="toe">Toe</span></p>
+        </div>
+        <div id="options">
+        <Options></Options>
+        </div>
+      </div>
     );
   }
 }
